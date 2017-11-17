e9698ad4450c2ae63cf201dff15d2d0f3e9d577e
Alexey <archon@ua.fm>
2017-11-17 13:48:49 +0200

    commit

--- db/StatisticsKeeper.java
+++ db/StatisticsKeeper.java
@@ -41,7 +41,7 @@ public class StatisticsKeeper {
             try{
                 connection = DriverManager.getConnection(
                         "jdbc:postgresql://127.0.0.1:5432/CalculatorDBstatistic", "postgres",
-                        "pfs222");
+                        "***");
 
 
 
